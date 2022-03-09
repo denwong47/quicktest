6620c18363e99cbb8212720ada6b3f60a06363c6
denwong47 <85551679+denwong47@users.noreply.github.com>
2022-03-09 22:49:46 +0000

    Comments corrected

--- src/quicktest/classes.py
+++ src/quicktest/classes.py
@@ -88,9 +88,24 @@ class TestCase(unittest.TestCase):
         ]
         
         func will be called in form of
-            func(**tests["args"])
+            func(**test["args"]) for test in tests
         
-        to which
+        the return of which will be matched against test["answer"] with the following logic:
+        - if test["answer"] is a type, and
+            - test["answer"] is a subclass of Exception, it will assert either
+                - an Exception of subclass of test["answer"] be raised; or
+                - such an Exception was returned by func().
+            - test["answer"] is any other classes, it will assert either
+                - the return of func() be an instance of test["answer"]; or
+                - the return of func() be a type that is a subclass of test["answer"].
+        - if test["answer"] is a list, tuple or dict, use the unittest methods of assertListEqual, assertTupleEqual or assertDictEqual.
+            - TODO iterate through these to allow for float determinations
+            - TODO iterate through these to allow for subset etc
+        - if test["answer"] is a float or numpy float, assert values being close
+        - if test["answer"] is a numpy array of dtype
+            - float, assert values being close
+            - all others, assert values being equal
+        - if test["answer"] is a pandas DataFrame or Series, asser them being close using default arguments.
         """
 
         for _test in tests:
@@ -150,7 +165,7 @@ class TestCase(unittest.TestCase):
 
             elif (isinstance(_test["answer"], tuple)):
                 """
-                Case: LISTS
+                Case: TUPLES
                 """
                 _assertion = self.assertTupleEqual
 
